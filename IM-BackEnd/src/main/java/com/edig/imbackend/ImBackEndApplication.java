f7718c37d28853796037d1723783d044a0f25593
EdiGamez <laloluz29@gmail.com>
2024-05-28 18:43:13 -0600

    Back End Init

---
+++ IM-BackEnd/src/main/java/com/edig/imbackend/ImBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.edig.imbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImBackEndApplication.class, args);
+    }
+
+}
---
+++ IM-BackEnd/src/test/java/com/edig/imbackend/ImBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edig.imbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
